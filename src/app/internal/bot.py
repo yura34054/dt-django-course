6913303d815fca332c055a89a92212899d581025
yura34054 <ur34054@gmail.com>
2023-03-13 16:50:39 +0000

    Homework2

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -1,7 +1,6 @@
-import requests
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from app.internal.services.telegram_service import me, set_phone, start, update_user_phone
+from app.internal.services.telegram_service import bank_status, me, set_phone, start, update_user_phone
 from config.settings import TELEGRAM_BOT
 
 
@@ -14,6 +13,7 @@ def run() -> None:
     dispatcher.add_handler(CommandHandler("set_phone", set_phone))
     dispatcher.add_handler(MessageHandler(Filters.contact, update_user_phone))
     dispatcher.add_handler(CommandHandler("me", me))
+    dispatcher.add_handler(CommandHandler("bank_status", bank_status))
 
     updater.start_polling()
 
