11ad96be97a19b7db6a63e1989b195ebda64e9d8
yura34054 <ur34054@gmail.com>
2023-03-16 16:08:36 +0300

    unexpected endpoint me fix

--- src/app/internal/transport/rest/handlers.py
+++ src/app/internal/transport/rest/handlers.py
@@ -5,6 +5,7 @@ from app.internal.services.user_service import get_user, get_user_info
 
 def me(request, phone_number):
     user = get_user(phone_number=phone_number)
-    user_info = get_user_info(user)
+    if user is None:
+        return JsonResponse({}, status=403)
 
-    return JsonResponse(user_info, status=403 if len(user_info) == 0 else 200)
+    return JsonResponse(get_user_info(user), status=200)
