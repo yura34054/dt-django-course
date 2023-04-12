yura34054/dt-django-course

commit 7fa7a8f1b09902cb54a85b594f8353f05428a1d1

fixed database usage (might not launch now)

src/app/internal/models/bank_card.py
@@ -5,10 +5,10 @@ from app.internal.models.bank_account import BankAccount
 
 class BankCard(models.Model):
     bank_account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, blank=True)
+    card_id = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return f"{self.bank_account.owner.telegram_id}: {self.name}"
+        return f"{self.bank_account.owner.telegram_id}: {self.id}"
 
     class Meta:
         verbose_name = "Bank card"

src/app/internal/models/transaction.py
@@ -1,0 +1,22 @@
+from django.db import models
+
+from app.internal.models.bank_account import BankAccount
+from app.internal.models.bank_card import BankCard
+
+
+class Transaction(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=11, decimal_places=2)
+
+    card_from = models.ForeignKey(BankCard, blank=True, on_delete=models.CASCADE, related_name="card_from")
+    account_from = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="account_from")
+
+    card_to = models.ForeignKey(BankCard, blank=True, on_delete=models.CASCADE, related_name="card_to")
+    account_to = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="account_to")
+
+    def __str__(self):
+        return f"{self.time}: {self.account_from.name} {self.amount} to {self.account_to.name}"
+
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"

commit c8afb45241f9025c80bc64c71d9acc5f53ed8199

fixed some errors introduced in 7fa7a8f1 and added some tests

Makefile
@@ -18,9 +18,6 @@ collectstatic:
 dev:
 	python src/manage.py runserver localhost:8000
 
-bot:
-	python src/manage.py run_bot
-
 command:
 	python src/manage.py ${c}
 
@@ -32,6 +29,10 @@ debug:
 
 piplock:
 	pipenv install
+	sudo chown -R ${USER} Pipfile.lock
+
+piplock_dev:
+	pipenv install --dev
 	sudo chown -R ${USER} Pipfile.lock
 
 lint:

Pipfile
@@ -7,6 +7,7 @@ verify_ssl = true
 black = "~=23.1.0"
 isort = "~=5.10"
 flake8 = "~=4.0"
+pytest-django = "~=4.5.2"
 
 [packages]
 django = "~=4.0"

src/app/internal/decorators/telegram_decorators.py
@@ -7,9 +7,8 @@ logging.basicConfig(level=logging.INFO)
 
 def requires_phone(func):
     def wrapper(update, context):
-        user = user_service.get_user(telegram_id=update.message.from_user.id)
 
-        if user.phone_number == "":
+        if not user_service.is_phone_set(telegram_id=update.message.from_user.id):
             update.message.reply_text("To use this method you need to provide your phone (/set_phone)")
 
         else:

src/app/internal/models/__init__.py
@@ -1,0 +1,5 @@
+from .admin_user import AdminUser
+from .bank_account import BankAccount
+from .bank_card import BankCard
+from .transaction import Transaction
+from .user import User

src/app/internal/models/bank_card.py
@@ -8,7 +8,7 @@ class BankCard(models.Model):
     card_id = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return f"{self.bank_account.owner.telegram_id}: {self.id}"
+        return f"{self.bank_account.owner.telegram_id}: {self.card_id}"
 
     class Meta:
         verbose_name = "Bank card"

src/app/internal/models/transaction.py
@@ -8,15 +8,16 @@ class Transaction(models.Model):
     time = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=11, decimal_places=2)
 
-    card_from = models.ForeignKey(BankCard, blank=True, on_delete=models.CASCADE, related_name="card_from")
+    card_from = models.ForeignKey(BankCard, blank=True, null=True, on_delete=models.SET_NULL, related_name="card_from")
     account_from = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="account_from")
 
-    card_to = models.ForeignKey(BankCard, blank=True, on_delete=models.CASCADE, related_name="card_to")
+    card_to = models.ForeignKey(BankCard, blank=True, null=True, on_delete=models.SET_NULL, related_name="card_to")
     account_to = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="account_to")
 
     def __str__(self):
         return f"{self.time}: {self.account_from.name} {self.amount} to {self.account_to.name}"
 
     class Meta:
+        ordering = ["time"]
         verbose_name = "Transaction"
         verbose_name_plural = "Transactions"

src/app/internal/models/user.py
@@ -16,7 +16,7 @@ class User(models.Model):
             )
         ],
     )
-    friends = models.ManyToManyField("self", symmetrical=False, blank=True)
+    friends = models.ManyToManyField("self", symmetrical=False, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.telegram_id})"

src/app/internal/services/bank_service.py
@@ -1,28 +1,31 @@
 from django.db import transaction
 from django.db.models import F, Q
+from django.core.exceptions import ObjectDoesNotExist
 
-from app.internal.models.bank_account import BankAccount
-from app.internal.models.bank_card import BankCard
-from app.internal.models.transaction import Transaction
-from app.internal.models.user import User
+from app.internal.models import BankAccount, BankCard, Transaction, User
 
 
-def create_account(user_id, name):
+def create_account(telegram_id, name):
     """create new BankAccount object if name isn't taken, else return reply message"""
 
-    owner = User.objects.get(telegram_id=user_id)
+    owner = User.objects.get(telegram_id=telegram_id)
 
-    account, created = BankAccount.objects.get_or_create(owner=owner, name=name)
+    account, created = BankAccount.objects.get_or_create(
+        owner=owner,
+        name=name,
+        defaults={"money": 0},
+    )
+
     if not created:
         return f'Account "{name}" already exists'
 
     return f'Account "{name}" successfully created'
 
 
-def create_card(user_info, bank_account_name):
+def create_card(telegram_id, bank_account_name):
     """create new BankCard object"""
 
-    bank_account = BankAccount.objects.filter(Q(owner__telegram_id=user_info.id) & Q(name=bank_account_name))
+    bank_account = BankAccount.objects.filter(Q(owner__telegram_id=telegram_id) & Q(name=bank_account_name))
 
     if not bank_account.exists():
         return f'No account "{bank_account_name}" found'
@@ -34,100 +37,107 @@ def create_card(user_info, bank_account_name):
     return f'Card "{card.card_id}" successfully created'
 
 
-def get_account_info(owner_id, name) -> dict:
+def get_account_info(telegram_id, name) -> dict:
     """return info about account if it exists, else empty dict"""
 
-    account = BankAccount.objects.filter(Q(owner__telegram_id=owner_id) & Q(name=name))
-
-    if not account.exists():
+    try:
+        account = BankAccount.objects\
+            .filter(Q(owner__telegram_id=telegram_id) & Q(name=name))\
+            .get()
+    except ObjectDoesNotExist:
         return {}
 
     info = {
         "name": account.name,
         "money": account.money,
-        "cards": list((card["id"] for card in account.bankcard_set.values("name"))),
+        "cards": list((card["card_id"] for card in account.bankcard_set.values("card_id"))),
     }
 
     return info
 
 
-def get_accounts(owner_id):
+def get_accounts(telegram_id):
     """return info about all accounts associated with user"""
 
     return {
         account["name"]: account["money"]
-        for account in BankAccount.objects.values("name", "money").filter(owner__telegram_id=owner_id)
+        for account in BankAccount.objects.values("name", "money").filter(owner__telegram_id=telegram_id)
     }
 
 
 def send_money(account_from, account_to, amount):
-    account_from = F("money") - amount
-    account_to = F("money") + amount
+    account_from.money = F("money") - amount
+    account_to.money = F("money") + amount
 
     account_from.save(update_fields=("money",))
     account_to.save(update_fields=("money",))
 
 
 def send_money_account(owner_id, receiver_username, account_name, receiver_account_name, amount):
-    if User.objects.filter(Q(telegram_id=owner_id) & Q(friends__username=receiver_username)).exists:
+    if not User.objects.filter(Q(telegram_id=owner_id) & Q(friends__username=receiver_username)).exists:
         return f"You need to add {receiver_username} to your friend list first by using /add_friend"
 
     amount = round(float(amount), 2)
     if amount < 0:
         amount = 0
 
-    account_from = BankAccount.objects.select_for_update().filter(Q(owner__telegram_id=owner_id) & Q(name=account_name))
-
-    if not account_from.exists():
-        return f"Account {account_name} not found"
-
-    account_to = BankAccount.objects.select_for_update().filter(
-        Q(owner__username=receiver_username) & Q(name=receiver_account_name)
-    )
-
-    if not account_to.exists():
-        return f"Receiver account {receiver_account_name} not found"
-
     with transaction.atomic():
-        account_from = account_from.get()
-        account_to = account_to.get()
+
+        try:
+            account_from = BankAccount.objects\
+                .select_for_update()\
+                .filter(Q(owner__telegram_id=owner_id) & Q(name=account_name))\
+                .get()
+        except ObjectDoesNotExist:
+            return f"Account {account_name} not found"
 
         if account_from.money < amount:
             return "Not enough money"
 
-        send_money(account_from, account_to, amount)
+        try:
+            account_to = BankAccount.objects\
+                .select_for_update()\
+                .filter(Q(owner__username=receiver_username) & Q(name=receiver_account_name))\
+                .get()
+        except ObjectDoesNotExist:
+            return f"Receiver account {receiver_account_name} not found"
 
+        send_money(account_from, account_to, amount)
         Transaction.objects.create(amount=amount, account_from=account_from, account_to=account_to)
 
-    return f"Successfully sent {amount} to {receiver_username}"
+    return f"Successfully sent {amount} to @{receiver_username}"
 
 
 def send_money_card(owner_id, card_id, receiver_card_id, amount):
+
     amount = round(float(amount), 2)
     if amount < 0:
         amount = 0
 
-    card_from = BankCard.objects.select_for_update()\
-        .filter(card_id=card_id).select_related("bank_account__owner__telegram_id")
-
-    if not card_from.exists():
-        return f"Card {receiver_card_id} not found"
-
-    card_to = BankCard.objects.select_for_update()\
-        .filter(card_id=receiver_card_id).select_related("bank_account__owner")
-
-    if not card_to.exists():
-        return f"Receiver card {receiver_card_id} not found"
-
     with transaction.atomic():
-        card_from = card_from.get()
+
+        try:
+            card_from = BankCard.objects\
+                .select_for_update()\
+                .filter(card_id=card_id)\
+                .select_related("bank_account__owner")\
+                .get()
+        except ObjectDoesNotExist:
+            return f"Card {card_id} not found"
 
         if card_from.bank_account.owner.telegram_id != owner_id:
-            return f"You don't own {card_id}"
+            return f"You don't own card {card_id}"
 
-        card_to = card_to.get()
+        try:
+            card_to = BankCard.objects\
+                .select_for_update()\
+                .filter(card_id=receiver_card_id)\
+                .select_related("bank_account__owner")\
+                .get()
+        except ObjectDoesNotExist:
+            return f"Receiver card {receiver_card_id} not found"
 
-        if User.objects.filter(Q(telegram_id=owner_id) & Q(friends=card_to.bank_account.owner)).exists:
+        if not User.objects.filter(Q(telegram_id=owner_id) & Q(friends=card_to.bank_account.owner)).exists:
             return f"You need to add owner of {receiver_card_id} to your friend list first by using /add_friend"
 
         send_money(card_from.bank_account, card_to.bank_account, amount)
@@ -140,41 +150,45 @@ def send_money_card(owner_id, card_id, receiver_card_id, amount):
             account_to=card_to.bank_account,
         )
 
-    return f"Successfully sent {amount} to {receiver_card_id}"
+    return f"Successfully sent {amount} to card \"{receiver_card_id}\""
 
 
 def get_bank_statement_account(telegram_id, account_name):
-    account = BankAccount.objects\
-        .filter(Q(owner__telegram_id=telegram_id) & Q(name=account_name))
-
-    if not account.exists():
+    try:
+        account = BankAccount.objects\
+            .filter(Q(owner__telegram_id=telegram_id) & Q(name=account_name))\
+            .get()
+    except ObjectDoesNotExist:
         return f"Account {account_name} not found"
 
     transactions = Transaction.objects\
-        .filter(account_from=account)[100:]
+        .filter(account_from=account)\
+        .values("account_from__name", "account_to__name", "amount")
 
-    if not transactions.exists():
+    if not transactions:
         return f"No transactions for account {account_name} found"
 
-    return transactions.all(), account.money
+    return [{t['account_from__name']: (
+        t['account_to__name'],
+        round(float(t['amount']), 2)
+    )} for t in transactions]
 
 
 def get_bank_statement_card(telegram_id, card_id):
-    card = BankAccount.objects\
-        .filter(Q(bank_account__owner__telegram_id=telegram_id) & Q(card_id=card_id))\
-        .select_related("bank_account__owner", "bank_account__money")
-
-    if not card.exists():
+    try:
+        card = BankAccount.objects\
+            .filter(Q(bank_account__owner__telegram_id=telegram_id) & Q(card_id=card_id))\
+            .select_related("bank_account__owner", "bank_account__money")\
+            .get()
+    except ObjectDoesNotExist:
         return f"Card {card_id} not found"
 
-    card.get()
-
     if card.bank_account.owner.telegram_id != telegram_id:
-        return f"You don't own {card_id}"
+        return f"You don't own card {card_id}"
 
-    transactions = Transaction.objects.filter(card_from=card)[100:]
+    transactions = Transaction.objects.filter(card_from=card)
 
-    if not transactions.exists():
+    if not transactions:
         return f"No transactions for card {card_id} found"
 
-    return transactions.all()
+    return list(transactions), card.bank_account.money

src/app/internal/services/user_service.py
@@ -1,17 +1,27 @@
 from django.db import transaction
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 
 from app.internal.models.user import User
-from app.internal.models.transaction import Transaction
 
 
-def create_user(user_info) -> None:
-    User.objects.get_or_create(
-        telegram_id=user_info.id,
-        first_name=user_info.first_name,
-        last_name=user_info.last_name,
-        username=f"@{user_info.username}",
-    )
+def create_user(telegram_id, first_name, last_name='', username='') -> (User, bool):
+    try:
+        return User.objects.get(telegram_id=telegram_id), False
+
+    except ObjectDoesNotExist:
+        return User.objects.create(
+            telegram_id=telegram_id,
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+        ), True
+
+
+def is_phone_set(telegram_id) -> bool:
+    user = User.objects.filter(telegram_id=telegram_id).values('phone_number').get()
+
+    return False if user['phone_number'] == '' else True
 
 
 def update_user_phone(telegram_id, phone_number) -> None:
@@ -36,44 +46,44 @@ def get_user_info(telegram_id=None, phone_number=None) -> dict:
 
 
 def add_friend(telegram_id, friend_username):
-    user = User.objects.select_for_update().filter(telegram_id=telegram_id).select_related("friends")
-    friend = User.objects.filter(username=friend_username)
+    user = User.objects.select_for_update().filter(telegram_id=telegram_id).prefetch_related("friends")
+    try:
+        friend = User.objects\
+            .filter(username=friend_username)\
+            .get()
+    except ObjectDoesNotExist:
+        return f"User @{friend_username} not found"
 
-    if not friend.exists():
-        return f"User {friend_username} not found"
+    with transaction.atomic():
+        if user.filter(friends__username=friend_username).exists():
+            return f"@{friend_username} already in friends"
 
-    if user.filter(friends__username=friend_username).exists():
-        return f"{friend_username} already in friends"
-
-    friend = friend.get()
-    user = user.get()
-
-    user.friends.add(friend)
-    user.save(update_fields=("friends",))
-    return f"{friend_username} added to friends"
+        user = user.get()
+        user.friends.add(friend)
+        return f"@{friend_username} added to friends"
 
 
 def remove_friend(telegram_id, friend_username):
-    user = User.objects.select_for_update().filter(telegram_id=telegram_id).select_related("friends")
-    friend = User.objects.filter(username=friend_username)
+    user = User.objects.select_for_update().filter(telegram_id=telegram_id)
+    try:
+        friend = User.objects\
+            .get(username=friend_username)
+    except ObjectDoesNotExist:
+        return f"User @{friend_username} not found"
 
-    if not friend.exists():
-        return f"User {friend_username} not found"
+    with transaction.atomic():
+        if not user.filter(friends__username=friend_username).exists():
+            return f"@{friend_username} already not in friends"
 
-    if not user.filter(friends__username=friend_username).exists():
-        return f"{friend_username} already not in friends"
-
-    friend = friend.get()
-    user = user.get()
-
-    user.friends.remove(friend)
-    user.save(update_fields=("friends",))
-    return f"{friend_username} removed from friends"
+        user = user.get()
+        user.friends.remove(friend)
+        user.save()
+        return f"@{friend_username} removed from friends"
 
 
 def list_friends(telegram_id):
-    user = User.objects.filter(telegram_id=telegram_id).values("friends__username")
-    return list(user["friends__username"])
+    friends = User.objects.filter(telegram_id=telegram_id).values("friends__username")
+    return list((f['friends__username'] for f in friends))
 
 
 def get_interactions(telegram_id):

src/app/internal/transport/bot/bank_handlers.py
@@ -43,19 +43,19 @@ def add_account(update: Update, context: CallbackContext):
 
     name = update.message.text.split()[1]
 
-    update.message.reply_text(bank_service.create_account(update.message.from_user, name))
+    update.message.reply_text(bank_service.create_account(update.message.from_user.id, name))
 
 
 @requires_phone
 @logged
 def add_card(update: Update, context: CallbackContext):
-    if len(update.message.text.split()) != 3:
+    if len(update.message.text.split()) != 2:
         update.message.reply_text("Use this command with one parameter: /add_card {account name}")
         return
 
     bank_account_name = update.message.text.split()[1]
 
-    update.message.reply_text(bank_service.create_card(update.message.from_user, bank_account_name))
+    update.message.reply_text(bank_service.create_card(update.message.from_user.id, bank_account_name))
 
 
 @requires_phone
@@ -68,21 +68,29 @@ def send_money_account(update: Update, context: CallbackContext):
         )
         return
 
-    update.message.reply_text(bank_service.send_money(update.message.from_user.id, *update.message.text.split()[1:]))
+    update.message.reply_text(bank_service.send_money_account(
+        update.message.from_user.id,
+        *update.message.text.split()[1:])
+    )
 
 
 @requires_phone
 @logged
 def send_money_card(update: Update, context: CallbackContext):
-    if len(update.message.text.split()) != 5:
+    if len(update.message.text.split()) != 4:
         update.message.reply_text(
             "Use this command with three parameters: " "/send_money_card {your card id} {receiver card id} {amount}"
         )
         return
 
-    update.message.reply_text(bank_service.send_money(update.message.from_user.id, *update.message.text.split()[1:]))
+    update.message.reply_text(bank_service.send_money_card(
+        update.message.from_user.id,
+        *update.message.text.split()[1:])
+    )
 
 
+@requires_phone
+@logged
 def get_bank_statement_account(update: Update, context: CallbackContext):
     if len(update.message.text.split()) != 2:
         update.message.reply_text(
@@ -96,6 +104,8 @@ def get_bank_statement_account(update: Update, context: CallbackContext):
     )
 
 
+@requires_phone
+@logged
 def get_bank_statement_card(update: Update, context: CallbackContext):
     if len(update.message.text.split()) != 2:
         update.message.reply_text(

src/app/internal/transport/bot/user_handlers.py
@@ -7,7 +7,13 @@ from app.internal.services import user_service
 
 @logged
 def start(update: Update, context: CallbackContext):
-    user_service.create_user(update.message.from_user)
+    from_user = update.message.from_user
+    user_service.create_user(
+        from_user.id,
+        from_user.first_name,
+        from_user.last_name,
+        from_user.username,
+    )
     update.message.reply_text("Hi!")
 
 
@@ -73,7 +79,7 @@ def list_friends(update: Update, context: CallbackContext):
         update.message.reply_text("No friends found :(")
         return
 
-    update.message.reply_text("Your friends:\n" ", ".join(friends))
+    update.message.reply_text("Your friends:\n" + ", ".join(friends))
 
 
 def get_interactions(update: Update, context: CallbackContext):

src/app/migrations/0008_remove_bankcard_id_remove_bankcard_name_and_more.py
@@ -1,7 +1,7 @@
-# Generated by Django 4.2 on 2023-04-10 13:44
+# Generated by Django 4.2 on 2023-04-12 12:40
 
-import django.db.models.deletion
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -21,8 +21,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="bankcard",
             name="card_id",
-            field=models.AutoField(default=1, primary_key=True, serialize=False),
+            field=models.AutoField(default=None, primary_key=True, serialize=False),
             preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name="user",
+            name="friends",
+            field=models.ManyToManyField(null=True, to="app.user"),
         ),
         migrations.CreateModel(
             name="Transaction",
@@ -46,7 +51,8 @@ class Migration(migrations.Migration):
                     "card_from",
                     models.ForeignKey(
                         blank=True,
-                        on_delete=django.db.models.deletion.CASCADE,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
                         related_name="card_from",
                         to="app.bankcard",
                     ),
@@ -55,7 +61,8 @@ class Migration(migrations.Migration):
                     "card_to",
                     models.ForeignKey(
                         blank=True,
-                        on_delete=django.db.models.deletion.CASCADE,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
                         related_name="card_to",
                         to="app.bankcard",
                     ),
@@ -64,6 +71,7 @@ class Migration(migrations.Migration):
             options={
                 "verbose_name": "Transaction",
                 "verbose_name_plural": "Transactions",
+                "ordering": ["time"],
             },
         ),
     ]

src/pytest.ini
@@ -1,0 +1,2 @@
+[pytest]
+DJANGO_SETTINGS_MODULE = config.settings

src/app/internal/services/tests/test_bank_service.py
@@ -1,0 +1,17 @@
+import pytest
+
+from app.internal.services import bank_service, user_service
+from app.internal.models import User, BankAccount, BankCard, Transaction
+
+
+@pytest.mark.django_db
+def test_create_account():
+    user_service.create_user(1, 'first_name', username='username')
+
+    assert bank_service.create_account(1, 'account') == 'Account "account" successfully created'
+    assert bank_service.create_account(1, 'account') == 'Account "account" already exists'
+
+
+@pytest.mark.django_db
+def test_create_card():
+    pass

src/app/internal/services/tests/test_user_service.py
@@ -1,0 +1,68 @@
+import pytest
+
+from app.internal.services import user_service
+from app.internal.models import User
+
+
+@pytest.mark.django_db
+def test_create_user():
+    user, created = user_service.create_user(1, 'first_name', 'last_name', 'username')
+    assert User.objects.count() == 1
+    assert created
+    assert user.telegram_id == 1
+    assert user.first_name == 'first_name'
+    assert user.last_name == 'last_name'
+    assert user.username == 'username'
+
+    user_service.create_user(1, 'first_name')
+    assert User.objects.count() == 1
+
+    user_service.create_user(2, 'first_name')
+    assert User.objects.count() == 2
+
+
+@pytest.mark.django_db
+def test_user_phone():
+    user_service.create_user(1, 'first_name')
+    assert not user_service.is_phone_set(1)
+
+    user_service.update_user_phone(1, '71234567890')
+    assert user_service.is_phone_set(1)
+    assert User.objects.get(telegram_id=1).phone_number == '71234567890'
+
+
+@pytest.mark.django_db
+def test_get_user_info():
+    user_service.create_user(1, 'first_name', 'last_name', 'username')
+    user_service.update_user_phone(1, '71234567890')
+    assert user_service.get_user_info(1) == {
+        "first_name": 'first_name',
+        "last_name": 'last_name',
+        "username": 'username',
+        "phone_number": '71234567890',
+    }
+
+
+@pytest.mark.django_db
+def test_user_friends():
+    user_service.create_user(1, 'first_name', username='username_1')
+
+    assert user_service.add_friend(1, 'username_2') == "User @username_2 not found"
+    assert user_service.remove_friend(1, 'username_2') == "User @username_2 not found"
+    assert user_service.list_friends(1) == [None]
+
+    user_service.create_user(2, 'first_name', username='username_2')
+
+    assert user_service.remove_friend(1, 'username_2') == "@username_2 already not in friends"
+    assert user_service.add_friend(1, 'username_2') == "@username_2 added to friends"
+    assert user_service.add_friend(1, 'username_2') == "@username_2 already in friends"
+    assert user_service.list_friends(1) == ['username_2']
+
+    assert user_service.remove_friend(1, 'username_2') == "@username_2 removed from friends"
+    assert user_service.list_friends(1) == [None]
+    assert user_service.remove_friend(1, 'username_2') == "@username_2 already not in friends"
+
+
+@pytest.mark.django_db
+def test_user_get_interactions():
+    pass